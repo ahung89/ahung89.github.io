4618ca5825d262a8c80a5e2c201248c03cb81a6c
Andrew Hung <druphoria@utexas.edu>
2014-11-24 20:24:34 -0800

    finally got some gulp stuff working

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,59 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglifyjs'),
-	browserify = require('gulp-browserify'),
+	uglify = require('gulp-uglify'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	buffer = require('vinyl-buffer'),
+	connect = require('gulp-connect'),
+	source = require('vinyl-source-stream'),
 	jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-	gulp.src(['js/lib/**/*.js', 'js/game/states/LevelState.js', 'js/game/**/*.js', 'js/Main.js'])
-		// .pipe(browserify())
-		.pipe(uglify('AndrewWorld.min.js'))
-		.pipe(gulp.dest('./dist'))
+gulp.task('default', ['connect', 'copy', 'copylibs', 'compile']);
+
+paths = {
+	assets: 'assets/**/*',
+	libs: [
+		'bower_components/phaser/build/phaser.js'
+	],
+	js: ['js/Main.js', 'js/game/**/*.js'],
+	entry: './js/Main.js',
+	dist: './dist/'
+};
+
+gulp.task('compile', function() {
+	var bundler = browserify({
+		cache: {}, packageCache: {}, fullPaths: true,
+		entries:[paths.entry]
+	});
+
+	var bundle = function() {
+		return bundler
+			.bundle()
+			.pipe(source('AndrewWorld.min.js'))
+			.pipe(buffer())
+			.pipe(uglify())
+			.pipe(gulp.dest(paths.dist))
+	}
+
+	bundler = watchify(bundler);
+	bundler.on('update', bundle);
+
+	return bundle();
+});
+
+gulp.task('copylibs', function() {
+	gulp.src(paths.libs)
+		.pipe(gulp.dest(paths.dist + 'js/lib'))
+});
+
+gulp.task('copy', function() {
+	gulp.src(paths.assets)
+		.pipe(gulp.dest(paths.dist + 'assets'))
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root: [__dirname],
+		port: 9000,
+		livereload: true
+	});
 });
